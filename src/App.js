e150b58bb553ea148a767f893de8531bc258529b
programmer-shaheb <abidurrahman471@gmail.com>
2021-12-11 12:54:49 +0600

    navbar & homepage layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import { Layout, Space, Typography } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
+
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h3>Go Crypto</h3>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Copyright © {new Date().getFullYear()}
+            <Link to="/"> Go Crypto Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
