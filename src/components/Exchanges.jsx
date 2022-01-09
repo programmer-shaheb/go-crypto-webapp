4d81db67b5812857f60ab95d82a061d7900a8e6f
programmer-shaheb <abidurrahman471@gmail.com>
2022-01-09 21:12:54 +0600

    ready for deploy

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -11,43 +11,55 @@ const Exchanges = () => {
   const { data, isFetching } = useGetCryptoExchangesQuery();
   const exchangesList = data?.data?.exchanges;
 
-if(isFetching) return <div className="loader" ><Spin/></div> 
+  if (isFetching)
+    return (
+      <div className="loader">
+        <Spin />
+      </div>
+    );
 
   return (
     <>
-    <Row>
-      <Col span={6}>Exchanges</Col>
-      <Col span={6}>24h Trade Volume</Col>
-      <Col span={6}>Markets</Col>
-      <Col span={6}>Change</Col>
-    </Row>
-    <Row>
-      {exchangesList.map((exchange) => (
-        <Col span={24}>
-          <Collapse>
-            <Panel
-              key={exchange.id}
-              showArrow
-              header={(
-                <Row key={exchange.id} >
-                  <Col span={6}>
-                    <Text><strong>{exchange.rank}.</strong></Text>
-                    <Avatar className="exchange-image" src={exchange.iconUrl} />
-                    <Text><strong>{exchange.name}</strong></Text>
-                  </Col>
-                  <Col span={6}>${millify(exchange.volume)}</Col>
-                  <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
-                  <Col span={6}>{millify(exchange.marketShare)}%</Col>
-                </Row>
-                )}
-            >
-              {HTMLReactParser(exchange.description || '')}
-            </Panel>
-          </Collapse>
-        </Col>
-      ))}
-    </Row>
-  </>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={
+                  <Row key={exchange.id} style={{ width: "100%" }}>
+                    <Col span={6}>
+                      <Text>
+                        <strong>{exchange.rank}.</strong>
+                      </Text>
+                      <Avatar
+                        className="exchange-image"
+                        src={exchange.iconUrl}
+                      />
+                      <Text>
+                        <strong>{exchange.name}</strong>
+                      </Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                  </Row>
+                }
+              >
+                {HTMLReactParser(exchange.description || "")}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
